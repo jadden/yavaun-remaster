ba9f9503dd8f51d30a0d23cdb270cdf6b1a290f8
Appsteroïd <contact@appsteroid.fr>
2024-11-25 20:21:48 +0100

    Fix super().call from units

--- ShamaLi/Scripts/Units/Worker.gd
+++ ShamaLi/Scripts/Units/Worker.gd
@@ -1,6 +1,6 @@
 extends BaseUnit
 
 func _ready():
-	BaseUnit._ready()  # Appelle la méthode `_ready()` de BaseUnit
+	super._ready()  # Appelle la méthode `_ready()` de BaseUnit
 	# Vos initialisations supplémentaires ici
 	print("Worker _ready() appelé.")
